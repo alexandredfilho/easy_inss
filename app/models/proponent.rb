dbac4ce5edad170871960278a292500ebb66185d
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-12-01 23:58:47 -0300

    Remove logic from the proponent model

--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -1,44 +1,14 @@
 # frozen_string_literal: true
 
 class Proponent < ApplicationRecord
-  belongs_to :user
-  has_one :salary, dependent: :destroy
-  has_one :address, dependent: :destroy
-  has_many :phones, dependent: :destroy
+  belongs_to :user, inverse_of: :proponents
+  has_one :salary, dependent: :destroy, inverse_of: :proponent
+  has_one :address, dependent: :destroy, inverse_of: :proponent
+  has_many :phones, dependent: :destroy, inverse_of: :proponent
 
   accepts_nested_attributes_for :salary, :address, :phones, allow_destroy: true
 
   validates :cpf, presence: true, cpf: true
-
-  after_initialize :initialize_associations, if: :new_record?
-
-  def calculate_inss
-    salary = params[:salary].to_f
-    inss_discount = Salary.new(amount: salary).calculate_inss
-    render json: { inss_discount: inss_discount }
-  end
-
-  def initialize_associations
-    build_address
-    build_phones
-    build_salary
-  end
-
-  private
-
-  def build_address
-    build_address if address.nil? || address.street.blank? || address.number.blank?
-  end
-
-  def build_phones
-    return unless phones.nil? || phones.empty?
-
-    phones.build
-  end
-
-  def build_salary
-    build_salary if salary.nil?
-  end
 end
 
 # == Schema Information
