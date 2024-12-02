2ccb74b827c3daf66541aec90ccd1f5d7c437cda
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-12-02 00:00:24 -0300

    Create the correct logic to calculate the aliquot

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -1,28 +1,48 @@
 # frozen_string_literal: true
 
 class Salary < ApplicationRecord
-  belongs_to :proponent
+  belongs_to :proponent, inverse_of: :salary
 
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :aliquot, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+
+  after_create :calculate_and_update_aliquot
 
   def calculate_inss
     salary = amount
     inss_discount = 0.0
 
-    inss_discount = case salary
-                    when 0..1412.00
-                      salary * 0.075
-                    when 1412.01..2666.68
-                      1412.00 * 0.075 + (salary - 1412.00) * 0.09
-                    when 2666.69..4000.03
-                      1412.00 * 0.075 + (2666.68 - 1412.00) * 0.09 + (salary - 2666.68) * 0.12
-                    else
-                      1412.00 * 0.075 + (2666.68 - 1412.00) * 0.09 + (4000.03 - 2666.68) * 0.12 +
-                      (salary - 4000.03) * 0.14
-                    end
+    salary_brackets.each do |bracket|
+      inss_discount += calculate_discount_for_bracket(salary, bracket)
+      salary = adjust_salary_for_next_bracket(salary, bracket)
+    end
 
     inss_discount.round(2)
+  end
+
+  private
+
+  def salary_brackets
+    [
+      { range: 0..1412.00, rate: 0.075 },
+      { range: 1412.01..2666.68, rate: 0.09 },
+      { range: 2666.69..4000.03, rate: 0.12 },
+      { range: 4000.04..7786.02, rate: 0.14 }
+    ]
+  end
+
+  def calculate_discount_for_bracket(salary, bracket)
+    salary_in_range = [salary - bracket[:range].begin, bracket[:range].end - bracket[:range].begin].min
+    salary_in_range * bracket[:rate]
+  end
+
+  def adjust_salary_for_next_bracket(salary, bracket)
+    [salary - bracket[:range].begin, 0].max
+  end
+
+  def calculate_and_update_aliquot
+    self.aliquot = calculate_inss
+    save
   end
 end
 
