93d475e353aee705495b42712a119ed3a8b3c08f
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-12-03 03:29:38 -0300

    Create the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+require 'tty-spinner'
+
+def create_user(spinner)
+  spinner.auto_spin
+  @user = User.first || User.create!(email: 'admin@domain.com', password: 'VerySecure@2024')
+end
+
+def create_proponents(spinner)
+  spinner.auto_spin
+
+  Proponent.skip_callback(:create, :after, :enqueue_inss_discount_metrics_job)
+  Proponent.skip_callback(:update, :after, :enqueue_inss_discount_metrics_job)
+  Proponent.skip_callback(:destroy, :after, :enqueue_inss_discount_metrics_job)
+
+  proponents = []
+
+  begin
+    10.times do
+      proponents << @user.proponents.create!(
+        name: Faker::Name.name,
+        cpf: Faker::IdNumber.brazilian_citizen_number(formatted: true),
+        date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
+        address_attributes: {
+          street: Faker::Address.street_name,
+          number: Faker::Address.building_number,
+          neighborhood: Faker::Address.community,
+          city: Faker::Address.city,
+          state: Faker::Address.state_abbr,
+          zip_code: Faker::Address.zip_code
+        },
+        phones_attributes: [
+          {
+            number: Faker::PhoneNumber.cell_phone_in_e164,
+            phone_type: %w[reference].sample,
+            reference_name: Faker::Name.name
+          }
+        ],
+        salary_attributes: {
+          amount: Faker::Commerce.price(range: 1200.0..7700.0)
+        }
+      )
+    end
+  ensure
+    Proponent.set_callback(:create, :after, :enqueue_inss_discount_metrics_job)
+    Proponent.set_callback(:update, :after, :enqueue_inss_discount_metrics_job)
+    Proponent.set_callback(:destroy, :after, :enqueue_inss_discount_metrics_job)
+  end
+
+  UpdateInssDiscountMetricsJob.perform_async(@user.id)
+end
+
+spinners = TTY::Spinner::Multi.new('[:spinner] Initializing database preparation!')
+
+create_user(spinners.register('[:spinner] Creating a user...'))
+create_proponents(spinners.register('[:spinner] Creating a proponents...'))
