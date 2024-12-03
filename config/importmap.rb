ca62c96b68e822e5cf159035b48aa5cfab60672b
Alexandre Domingos Filho <alexandre.dfilho@gmail.com>
2024-12-03 03:28:38 -0300

    Use importmap to handle dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,12 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js'
+pin '@hotwired/stimulus', to: '@hotwired--stimulus.js' # @3.2.2
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin 'stimulus-use'
+pin 'chartkick', to: 'chartkick.js'
+pin 'Chart.bundle', to: 'Chart.bundle.js'
+pin 'bootstrap' # @5.3.3
+pin '@popperjs/core', to: '@popperjs--core.js'
